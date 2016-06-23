e8acb0e53dc04f1804ca023deda0a6691cd38f4c
RedbackThomson <redbackthomson@gmail.com>
2016-06-23 23:24:33 +0800

    Font and description

--- public/src/App.js
+++ public/src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import Description from './Description.js'
 
 export default class App extends Component {
   render() {
+    var current_description = "hello";
     return (
-      <h1 className="hero">Hello, world.</h1>
+      <div className="container">
+        <header className="hero">
+          <h1 className="hero__name">
+            <span className="hero__name--redback">Redback</span> 
+            <span className="hero__name--thomson">Thomson</span> 
+          </h1>
+          <Description></Description>
+        </header>
+      </div>
     );
   }
 }
---
+++ public/src/Description.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+export default class Description extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentDescriptions: []
+    };
+    this.descriptionPeriod = 2500;
+    this.descriptionIndex = -1;
+    this.descriptions = [
+      'Web Developer',
+      'Programmer',
+      'Student',
+      'Tinkerer',
+      'Enthusiast'
+    ];
+  }
+
+  componentDidMount() {
+    this.startDescriptionTimer();
+  }
+
+  componentWillUnmount() {
+    if(this._descriptionTimer) {
+      clearInterval(this._descriptionTimer);
+      this._descriptionTimer = null;
+    }
+  }
+
+  startDescriptionTimer() {
+    this._descriptionTimer = setInterval(this.changeDescription.bind(this), this.descriptionPeriod);
+  }
+
+  changeDescription() {
+    this.descriptionIndex = (this.descriptionIndex + 1) % this.descriptions.length;
+    var newDescriptions = [this.descriptions[this.descriptionIndex]];
+    this.setState({currentDescriptions: newDescriptions});
+  }
+
+  render() {
+    var descriptions = this.state.currentDescriptions.map(function(item, i) {
+      return (<div className="hero__description" key={item}> { item } </div>);
+    });
+    return (
+      <ReactCSSTransitionGroup 
+        transitionName="description-shift" 
+        transitionAppear={true}
+        transitionEnterTimeout={500}
+        transitionAppearTimeout={500} 
+        transitionLeaveTimeout={500}>
+        {descriptions}
+      </ReactCSSTransitionGroup>
+    );
+  }
+}

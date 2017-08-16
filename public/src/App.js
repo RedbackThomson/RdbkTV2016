c05557a2e2adf370fa3cf31f01ae9cd971c776d9
RedbackThomson <redbackthomson@gmail.com>
2017-08-15 22:53:43 -0700

    Fixed foundation version

--- public/src/App.js
+++ public/src/App.js
@@ -39,7 +39,7 @@ export default class App extends Component {
       duration: 100,
       file: 'img/rblogo-outline.svg'
     }, function(obj) {
-      obj.el.classList.add('hero__logo--finished');
+      obj.parentEl.classList.add('hero__logo--finished');
     }).play();
   }
 
